71aad0ef05fbc337630d6a2a19113be939c3445a
nguyevankhai1 <nguyenvankhai01022000@gmail.com>
2024-01-08 14:20:37 +0700

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist', // Thư mục đầu ra build
+    assetsDir: 'assets', // Thư mục chứa tài nguyên (hình ảnh, font chữ, ...)
+    minify: 'terser', // Minify bằng Terser (có thể thay đổi thành 'esbuild' nếu bạn muốn sử dụng esbuild)
+    sourcemap: true, // Tạo source map
+    rollupOptions: {
+      // Cấu hình cho Rollup
+      external: ['vue'] // Bạn có thể thêm các dependencies bạn muốn giữ ngoại trừ ở đây
     }
   }
 })
